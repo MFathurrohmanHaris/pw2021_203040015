c07ac89212e83de0345df83afe7ee9609cc252f5
MFathurrohmanHaris <harisfathurrohman142@gmail.com>
2021-05-20 21:53:59 +0700

    cek

--- praktikum/modul1/latihan1a.php
+++ praktikum/modul1/latihan1a.php
@@ -1,12 +1,13 @@
-<?php 
+<?php
 /*
 Muhammad Fathurrohman Haris
 203040015
 Rabu jam 9
 */
-for($i = 1; $i <= 3; $i++){
-    for($j = 1; $j <= 3; $j++){
+for ($i = 1; $i <= 3; $i++) {
+    for ($j = 1; $j <= 3; $j++) {
         echo "Ini perulangan ke ($i,$j)";
         echo "<br>";
     }
 }
+// cek
